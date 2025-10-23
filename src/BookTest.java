09690a7ff379db747a9ac590ef6aeed8301fb629
Hiranur İleyna Bilir <hiranurbilir0@gmail.com>
2025-10-23 14:05:04 +0300

    unit commit

--- src/BookTest.java
+++ src/BookTest.java
@@ -1,2 +1,10 @@
 public class BookTest {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book b2 = new Book(" Java 101 ", " James Gosling ", 150.0);
+
+        b1.showDetails();
+        b2.showDetails();
+
+    }
 }
